d780ba1c2c462176f7b5b240427eb36c7e4e9f37
eduardamirelly <mirelly.eduarda404@gmail.com>
2022-05-22 18:06:49 -0300

    :construction: front-end setup: font and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import TodoList from './pages/TodoList'
 
 function App() {
   return (
-    <div>
-      <h1 className='text-xl text-center text-cyan-500'>Hello World!</h1>
-    </div>
+    <>
+      <TodoList />
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': {
+          900: '#241722',
+          800: '#352432',
+          700: '#462730',
+          600: '#632329',
+        },
+        'primary': '#F25551',
+        'second': {
+          100: '#FFFBFF',
+          200: '#FEEDE1',
+        }
+      },
+    },
   },
   plugins: [],
 }
